612de66ab173acbebbe95410f048d9a39321a888
EfrenPB <efren@whisper.sh>
2021-03-22 22:26:17 -0600

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Efrén Pérez Bernabe on 22/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
